570c80eb6a3026c7b03a3f01c357b75fb8ad9374
Leon <85785527+Leon-aka-10@users.noreply.github.com>
2025-02-07 10:39:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,6 @@ def classify_given_number():
         return jsonify({"error": True, "message": f"Error processing number: {error}"}), 500
 
 
-   # Running the Flask application
-   if __name__ == '__main__':
-       app.run(debug=False, host='0.0.0.0', port=5001)
+       # Running the Flask application
+       if __name__ == '__main__':
+           app.run(debug=False, host='0.0.0.0', port=5001)
